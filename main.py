624b2227141103245d514365c7bc087a6df6baf1
Gearmind-student <plannerpalhelp@gmail.com>
2023-03-17 19:33:57 -0500

    added login page, added index page

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello print")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
